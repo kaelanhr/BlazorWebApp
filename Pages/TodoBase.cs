27e5961043c3a246a27e823ca7e1d2db88500538
Kael <kaelanreece@gmail.com>
2019-10-07 21:26:44 +1000

    added parameter routing and code behind structure for blazor files. Update todo to use code behind structure

---
+++ Pages/TodoBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BlazorWebApplication1;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSample
+{
+	public class TodoBase : ComponentBase
+	{
+		protected IList<TodoItem> todos = new List<TodoItem>();
+		protected string newTodo;
+		protected string errors;
+
+		public void AddTodo()
+		{
+			// if we have something in the input field, add it to list and 
+			// reset input field
+			if (!string.IsNullOrWhiteSpace(newTodo))
+			{
+				todos.Add(new TodoItem { Title = newTodo });
+				newTodo = string.Empty;
+				errors = "";
+			}
+			else
+			{
+				errors = "Field cannot be null, empty or whitespace";
+			}
+		}
+	}
+}
