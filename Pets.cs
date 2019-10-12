860ee7f9f4af877bf92e38a025d0a058a0824887
Kaelan Reece <kaelanreece@gmail.com>
2019-10-12 22:07:32 +1000

    added pets class and table template

---
+++ Pets.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorWebApplication1
+{
+	public class Pets
+	{
+		public Pets(string petId, string name)
+		{
+			PetId = petId;
+			Name = name;
+		}
+
+		public string PetId { get; set; }
+		public string Name { get; set; }
+
+	}
+}
