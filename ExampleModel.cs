ce22ba10ba6222d5f19f920641647417bad85b94
Kael <kaelanreece@gmail.com>
2019-10-15 21:26:27 +1000

    added forms validation 👌

---
+++ ExampleModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+	public class ExampleModel
+	{
+		[Required]
+		[StringLength(10, ErrorMessage = "Name is too long.")]
+		public string Name { get; set; }
+	}
